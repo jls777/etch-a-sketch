bfb2cc2fcc965a5395db420c12db31636c1fb138
John Sison <jlsison777@gmail.com>
2023-09-02 11:08:16 -0400

    added add square function

--- eas.js
+++ eas.js
@@ -1,25 +1,31 @@
 const container = document.querySelector('.container');
 
 const squareContDiv = document.createElement('div');
+const squareDiv = document.createElement('div');
 
+
+// -----------function------------------
 function addContainter(num = 16) {
-  let cloneCont = squareContDiv.cloneNode(true);
   for (i = 0; i < num; i += 1) {
     cloneContDiv = squareContDiv.cloneNode(true);
     container.append(cloneContDiv);
+    squareContDiv.classList.add('sqr-cont');
   }
 }
 addContainter(); //to add containers for squares
-squareContDiv.classList.add('sqr-cont') //class for square containers
+// ----------------------
 
+//----------function for adding squares-----------------
+const squareContainer = document.querySelectorAll('.sqr-cont');
+function addSquares(num = 16) {
+  squareContainer.forEach(div => {
+    for (i = 0; i < num; i += 1) {
+      cloneSquares = squareDiv.cloneNode(true);
+      div.append(cloneSquares);
+      squareDiv.classList.add('square')
+    }
+  })
+}
+addSquares();
+//---------------------------------------------------
 
-
-
-// const squareDiv1 = document.createElement('div');
-// container.append(squareDiv1);
-// squareDiv1.classList.add('column');
-// const columnDiv = document.querySelector('.column');
-
-// const squareDiv2 = document.createElement('div');
-// squareDiv2.classList.add('column');
-// columnDiv.append(squareDiv2);

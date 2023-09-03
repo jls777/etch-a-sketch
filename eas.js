5147c5eaaadcd7667c6a0df35fa72da1bc7fdc75
John Sison <jlsison777@gmail.com>
2023-09-03 18:38:01 -0400

    added promp button that clear the current grid and add new ones

--- eas.js
+++ eas.js
@@ -1,31 +1,60 @@
-const container = document.querySelector('.container');
-
+let container = document.querySelector('.container');
+let bodyContainer = document.querySelector('.body-container');
+let header = document.querySelector('.header');
+let footer = document.querySelector('.footer');
+//-----------------------------------------
 const squareContDiv = document.createElement('div');
 const squareDiv = document.createElement('div');
 squareContDiv.classList.add('sqr-cont');
 squareDiv.classList.add('square')
-// -----------function------------------
-function addContainter(num = 16) {
-  for (i = 0; i < num; i += 1) {
+
+// -----------function-for adding squares----------------
+function addSquares(row = 16, column = 16) {
+  for (i = 0; i < row; i += 1) {
     cloneContDiv = squareContDiv.cloneNode(true);
     squareContDiv.classList.add('sqr-cont');
     container.append(cloneContDiv);
-    
   }
-}
-addContainter(100); //to add containers for squares
-// ----------------------
-
-//----------function for adding squares-----------------
-const squareContainer = document.querySelectorAll('.sqr-cont');
-function addSquares(num = 16) {
+  let squareContainer = document.querySelectorAll('.sqr-cont');
   squareContainer.forEach(div => {
-    for (i = 0; i < num; i += 1) {
+    for (i = 0; i < column; i += 1) {
       cloneSquares = squareDiv.cloneNode(true);
       div.append(cloneSquares);
     }
   })
 }
-addSquares(100); //to add squares
-//---------------------------------------------------
+addSquares()
+//--------------------------------------------------------
+const promptButton = document.querySelector('.prompt-button');
+promptButton.addEventListener('click', e => {
+  let container = document.querySelector('.container');
+  
+  container.remove()
+  
+  let row = window.prompt('enter row number');
+  let column = window.prompt('enter column number');
 
+  let newContainer = document.createElement('div');
+  bodyContainer.insertBefore(newContainer, footer);
+  newContainer.classList.add('container');
+  
+  
+  
+  function newAddSquares(row, column) {
+    for (i = 0; i < row; i += 1) {
+      cloneContDiv = squareContDiv.cloneNode(true);
+      squareContDiv.classList.add('sqr-cont');
+      newContainer.append(cloneContDiv);
+    }
+    let squareContainer = document.querySelectorAll('.sqr-cont');
+    squareContainer.forEach(div => {
+      for (i = 0; i < column; i += 1) {
+        cloneSquares = squareDiv.cloneNode(true);
+        div.append(cloneSquares);
+      }
+    })
+  }
+  newAddSquares(row, column);
+
+  
+})
